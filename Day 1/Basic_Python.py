406aa4a5b118d96910d59651c04a93472e73cb99
Hridoy Debnath <hridoydebnath111@gmail.com>
2025-11-18 15:58:06 +0600

    @nd Day code

---
+++ Day 1/Basic_Python.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+print("NumPy OK, version:", np.__version__)
+print("Pandas OK, version:", pd.__version__)
+
+plt.plot([1,2,3], [1,4,9])
+plt.title("Test Plot")
+plt.show()
