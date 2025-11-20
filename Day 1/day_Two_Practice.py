f8aecd14f30201417bfd466662b40f190615d991
Hridoy Debnath <hridoydebnath111@gmail.com>
2025-11-20 18:17:54 +0600

    Practice day2

---
+++ Day 1/day_Two_Practice.py
@@ -0,0 +1,16 @@
+a = 10
+b = 20
+
+print("Before Swap:", a, b)
+
+temp = a
+a = b
+b = temp
+
+print("After Swap:", a, b)
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
